b585954ce4bc23b9d448c17f54afb1e0efde3d04
Helena Wu <Helena.wu87@gmail.com>
2016-05-24 14:56:49 -0700

    seekBar directive, drag logic with jQuery

---
+++ dist/scripts/directives/seekBar.js
@@ -0,0 +1,58 @@
+(function(){
+    function seekBar($document){
+        var calculatePercent = function(seekBar, event){
+            var offsetX = event.pageX - seekBar.offset().left;
+            var seekBarWidth = seekBar.width();
+            var offsetXPercent = offsetX / seekBarWidth;
+            offsetXPercent = Math.max(0, offsetXPercent);
+            offsetXPercent = Math.min(1, offsetXPercent);
+            return offsetXPercent;
+        }
+        
+        return {
+            templateUrl: '/templates/directives/seek_bar.html',
+            replace: true,
+            restrict: 'E',
+            scope: {}, //new scope for this directive, not parent scope
+            link: function(scope, element, attributes){
+                //params strict order, element is jqlite wrapped
+                //directive logic, register DOM listeners
+                scope.value = 0;
+                scope.max = 100;
+                
+                var seekBar = $(element);
+                var percentString = function(){
+                    var value = scope.value;
+                    var max = scope.max;
+                    var percent = value / max * 100;
+                    return percent + "%";
+                }
+                
+                scope.fillStyle = function(){
+                    return {width: percentString()};
+                };
+                scope.onClickSeekBar = function(event){
+                    var percent = calculatePercent(seekBar,event);
+                    scope.value = percent * scope.max;
+                };
+                scope.trackThumb = function(){
+                    $(document).bind('mousemove.thumb', function(event){
+                        var percent = calculatePercent(seekBar, event);
+                        scope.$apply(function(){
+                            console.log("triggered");
+                            scope.value = percent * scope.max;
+                        });
+                    });
+                    $(document).bind('mouseup.thumb', function(){
+                        $(document).unbind('mousemove.thumb');
+                        $(document).unbind('mouseup.thumb');
+                    });
+                }
+            }
+        };
+    }
+    
+    angular    
+        .module('blocJams')
+        .directive('seekBar', ["$document", seekBar]);
+})();
